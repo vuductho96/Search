fd7ea52eef2979855d694e66e5c4ef03a5bb1586
vuductho96 <112962875+vuductho96@users.noreply.github.com>
2023-04-02 12:30:29 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,8 +13,8 @@ namespace Search
             for (int i = 0; i < students.Length; i++)
             {
                 if (students[i].Equals(input_name))
-                {
-                    Console.WriteLine("Position of the students in the list  [" + input_name + " ] is: " + (i + 1));
+                {Console.WriteLine ("Found !");
+                    Console.WriteLine("Position of the students in the list  [" + input_name + " ] is: " + i);
                     isExist = true;
                     break;
                 }
